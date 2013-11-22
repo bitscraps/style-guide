09be3a8d131dbefc70c127c04ea7115ef6c17403
Graham Hadgraft <graham.hadgraft@stylefoundry.co.uk>
2013-11-22 15:59:38 +0000

    initial importing:

--- microsite/style-guide.js
+++ microsite/style-guide.js
@@ -1,1 +1,16 @@
-style-guide.js
+function createLinks() {
+  links = document.getElementsByClassName('website-link');
+  console.log(links);
+
+  var len=links.length;
+  for(var i=0; i<len; i++) {
+    var this_link = links[i];
+    console.log(link);
+
+    this_link.onclick = function(this_link) {
+      document.getElementById(this_link.href).style.display = 'block';
+    };
+  };
+
+
+}
